33ee8060a5a18052b044bcfa24e7b54a6681deef
josephattieh <40753763+josephattieh@users.noreply.github.com>
2021-11-22 02:50:27 +0200

    Update Main.java

--- src/com/josephattieh/project/Main.java
+++ src/com/josephattieh/project/Main.java
@@ -6,7 +6,7 @@ import javafx.scene.Scene;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
-public class M extends Application{
+public class Main extends Application{
 
 	public static void main(String[] args) {
 		launch(args); 
